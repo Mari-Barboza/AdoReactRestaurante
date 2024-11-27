604adcc5699130a2a54e90f19050a83477296715
Marina <126211716+Mari-Barboza@users.noreply.github.com>
2024-11-27 16:55:31 -0300

    Delete ProdutoRepository.java

--- ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.livraria.projeto_livraria.repository;
-
-import br.livraria.projeto_livraria.model.Produto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProdutoRepository extends MongoRepository<Produto, String> {
-    // Métodos de consulta personalizados, se necessário
-}
